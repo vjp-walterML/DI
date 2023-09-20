75fe2883c0e0f075364cedd8bff06774667cbd6f
1DAW - Walter Martin Lopes <wmartinl01@informatica.iesvalledeljerteplasencia.es>
2023-09-20 11:40:00 +0200

    clase

--- Juego_Uno/src/juego_uno/Carta.java
+++ Juego_Uno/src/juego_uno/Carta.java
@@ -1,5 +1,8 @@
 package juego_uno;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author wmartinl01
@@ -38,14 +41,80 @@ public class Carta {
     }
 
     //MÉTODOS PROPIOS
-    public void mostrarCarta() {
-        System.out.println(" -------------");
-        System.out.println("|             |");
-        System.out.println("|     " + numero);
-        System.out.println("|             |");
-        System.out.println("|   " + color);
-        System.out.println("|             |");
-        System.out.println(" -------------");
+    //Este método nos comprobará si la carta es jugable con la carta del centro de la mesa
+    public boolean valida(Carta cartaMesa) {
+        return this.color.equals(cartaMesa.getColor()) || this.numero == cartaMesa.getNumero();
     }
 
+    public void mostrarCarta() {
+        switch (color) {
+            case "ROJO":
+                System.out.println(Juego.rojo + " -----");
+                System.out.println(Juego.rojo + "|     |");
+                System.out.println(Juego.rojo + "|  " + numero + "  |");
+                System.out.println(Juego.rojo + "|     |");
+                System.out.println(Juego.rojo + " -----");
+                break;
+            case "AZUL":
+                System.out.println(Juego.azul + " -----");
+                System.out.println(Juego.azul + "|     |");
+                System.out.println(Juego.azul + "|  " + numero + "  |");
+                System.out.println(Juego.azul + "|     |");
+                System.out.println(Juego.azul + " -----");
+                break;
+            case "AMARILLO":
+                System.out.println(Juego.amarillo + " -----");
+                System.out.println(Juego.amarillo + "|     |");
+                System.out.println(Juego.amarillo + "|  " + numero + "  |");
+                System.out.println(Juego.amarillo + "|     |");
+                System.out.println(Juego.amarillo + " -----");
+                break;
+            case "VERDE":
+                System.out.println(Juego.verde + " -----");
+                System.out.println(Juego.verde + "|     |");
+                System.out.println(Juego.verde + "|  " + numero + "  |");
+                System.out.println(Juego.verde + "|     |");
+                System.out.println(Juego.verde + " -----");
+                break;
+            default:
+                throw new AssertionError();
+        }
+    }
+
+    public List<String> mostrarCartaUsuario() {
+        List<String> cartas = new ArrayList<>();
+        switch (color) {
+            case "ROJO":
+                cartas.add(Juego.rojo + " -----");
+                cartas.add(Juego.rojo + "|     |");
+                cartas.add(Juego.rojo + "|  " + numero + "  |");
+                cartas.add(Juego.rojo + "|     |");
+                cartas.add(Juego.rojo + " -----");
+                break;
+            case "AZUL":
+                cartas.add(Juego.azul + " -----");
+                cartas.add(Juego.azul + "|     |");
+                cartas.add(Juego.azul + "|  " + numero + "  |");
+                cartas.add(Juego.azul + "|     |");
+                cartas.add(Juego.azul + " -----");
+                break;
+            case "AMARILLO":
+                cartas.add(Juego.amarillo + " -----");
+                cartas.add(Juego.amarillo + "|     |");
+                cartas.add(Juego.amarillo + "|  " + numero + "  |");
+                cartas.add(Juego.amarillo + "|     |");
+                cartas.add(Juego.amarillo + " -----");
+                break;
+            case "VERDE":
+                cartas.add(Juego.verde + " -----");
+                cartas.add(Juego.verde + "|     |");
+                cartas.add(Juego.verde + "|  " + numero + "  |");
+                cartas.add(Juego.verde + "|     |");
+                cartas.add(Juego.verde + " -----");
+                break;
+            default:
+                throw new AssertionError();
+        }
+        return cartas;
+    }
 }
--- Juego_Uno/src/juego_uno/Juego.java
+++ Juego_Uno/src/juego_uno/Juego.java
@@ -1,6 +1,9 @@
 package juego_uno;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
 
 /**
  *
@@ -10,22 +13,105 @@ public class Juego {
 
     final static String[] COLORES = {"ROJO", "AZUL", "AMARILLO", "VERDE"};
     final static int[] NUMEROS = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-//    public static ArrayList<Carta> mazo = new ArrayList<>();
-//    public static ArrayList<Carta> cartasMesa = new ArrayList<>();
-//    public static ArrayList<Carta> mazoUsuario = new ArrayList<>();
-//    public static ArrayList<Carta> mazoMaquina = new ArrayList<>();
+    final static int CARTAS_REPARTIR = 7;
+    public static final String rojo = "\u001B[31m";
+    public static final String azul = "\u001B[34m";
+    public static final String amarillo = "\u001B[33m";
+    public static final String verde = "\u001B[32m";
+    public static boolean juegoTerminado = false;
+    public static List<Carta> mazo = new ArrayList<>();
+    public static List<Carta> mesa = new ArrayList<>();
+    public static List<Carta> cartasUsuario = new ArrayList<>();
+    public static List<Carta> cartasMaquina = new ArrayList<>();
 
     public static void main(String[] args) {
-        ArrayList<Carta> mazo = new ArrayList<>();
-        ArrayList<Carta> cartasMesa = new ArrayList<>();
-        ArrayList<Carta> mazoUsuario = new ArrayList<>();
-        ArrayList<Carta> mazoMaquina = new ArrayList<>();
+        System.out.println(rojo + "****************************************");
+        System.out.println(amarillo + "********* B I E N VE N I D O ***********");
+        System.out.println(amarillo + "********** A L   U N O   D E ***********");
+        System.out.println(amarillo + "************* W A L T E R **************");
+        System.out.println(rojo + "****************************************\n");
 
+        //Creamos el mazo e iniciamos el juego barajando y repartiendo las cartas.
         rellenarMazo();
-        mostrarMazo();
+        iniciarJuego();
+        mostrarCartas();
+//        while (!juegoTerminado) {
+//            //mostrarCartas();
+//            turnoUsuario();
+//            //mostrarCartas();
+//            comprobarJugada();
+//            turnoMaquina();
+//            comprobarJugada();
+//        }
     }
 
-    //Rellena el mazo
+    //Todas las acciones necesarias del turno de la máquina
+    public static void turnoMaquina() {
+        boolean jugada = false;
+        //Primero compruebo si el juego ha terminado, si no es asi actua
+        if (!juegoTerminado) {
+            for (Carta carta : cartasMaquina) {
+                if (carta.valida(mesa.get(mesa.size() - 1))) {
+                    mesa.add(carta);
+                    cartasMaquina.remove(carta);
+                    jugada = true;
+                    break;
+                }
+            }
+            if (!jugada) {
+                cartasMaquina.add(mazo.remove(0));
+                System.out.println("---- LA MÁQUINA HA ROBADO CARTA ----");
+            } else {
+                System.out.println("---- LA MÁQUINA HA JUGADO CARTA ----");
+            }
+        }
+    }
+
+    //Todas las acciones necesarias del turno del usuario
+    public static void turnoUsuario() {
+        Carta cartaElegida;
+        int carta;
+        if (!juegoTerminado) {
+            carta = pedirNumeroCarta();
+            //Compruebo si ha elegido la opción robar si es asi robamos y pasamos turno
+            if (carta == (cartasUsuario.size() + 1)) {
+                cartasUsuario.add(mazo.remove(0));
+                System.out.println("----- HAS ROBADO UNA CARTA -----");
+            } else {
+                cartaElegida = cartasUsuario.get(carta);
+                //Compruebo si el jugador puede jugar esa carta o debe realizar otra opcion
+                if (cartaElegida.valida(mesa.get(mesa.size() - 1))) {
+                    mesa.add(cartasUsuario.remove(carta));
+                } else {
+                    System.out.println("----- ¡NO PUEDES JUGAR ESA CARTA!, ¡ELIGE OTRA O ROBA! -----");
+                    turnoUsuario();
+                }
+            }
+        }
+    }
+
+    //Este método comprueba si se ha terminado el juego y quien es el ganador
+    public static void comprobarJugada() {
+        if (!juegoTerminado) {
+            // Condiciones de finalización
+            if (cartasUsuario.isEmpty() || cartasMaquina.isEmpty() || mazo.isEmpty()) {
+                juegoTerminado = true; // Termina el juego
+
+                if (cartasUsuario.size() < cartasMaquina.size()) {
+                    System.out.println("----- ¡ENHORABUENA! ¡HAS GANADO! -----");
+                } else if (cartasUsuario.size() > cartasMaquina.size()) {
+                    System.out.println("----- ¡LO SIENTO! ¡LA MÁQUINA HA GANADO! -----");
+                    System.out.println("\n******************************************");
+                    System.out.println("*********** G A M E    O V E R ***********");
+                    System.out.println("******************************************");
+                } else {
+                    System.out.println("----- EMPATE -----");
+                }
+            }
+        }
+    }
+
+    //Rellena la lista de mazo con las respectivas cartas y luego las baraja.
     public static void rellenarMazo() {
         for (String c : COLORES) {
             for (int n : NUMEROS) {
@@ -33,41 +119,53 @@ public class Juego {
                 mazo.add(new Carta(n, c));
             }
         }
+        //Barajamos con el Collections.shuffle
+        Collections.shuffle(mazo);
     }
 
-    //Reparte 7 cartas a cada usuario, eliminandolas del mazo
-    public static void repartirCartas() {
-
-    }
-
-    //Muestra el mazo 
-    public static void mostrarMazo() {
-        for (Carta c : mazo) {
-            c.mostrarCarta();
+    //Reparte 7 cartas a cada usuario, eliminandolas del mazo y añade una al centro de la mesa
+    public static void iniciarJuego() {
+        for (int i = 0; i < CARTAS_REPARTIR; i++) {
+            cartasUsuario.add(mazo.remove(0));
+            cartasMaquina.add(mazo.remove(0));
         }
+        //Añadimos una carta al centro de la mesa
+        mesa.add(mazo.remove(0));
     }
 
-    //Muestra el mazo en filas de 4
-    public static void mostrarMazo4() {
-        Carta carta;
-        ArrayList<Carta> mazoAuxiliar = new ArrayList<>();
-        int contador = 0;
-        for (Carta c : mazo) {
-            carta = c;
-            mazoAuxiliar.add(c);
-            contador++;
-            if (contador == 4) {
-                System.out.println(" -------------     -------------     -------------     -------------");
-                System.out.println("|             |   |             |   |             |   |             |");
-                System.out.println("|     " + mazoAuxiliar.get(0).getNumero() + "       |         " + mazoAuxiliar.get(1).getNumero() + "       |         " + mazoAuxiliar.get(2).getNumero() + "       |         " + mazoAuxiliar.get(3).getNumero());
-                System.out.println("|             |   |             |   |             |   |             |");
-                System.out.println("|   " + mazoAuxiliar.get(0).getColor() + "     |       " + mazoAuxiliar.get(1).getColor() + "     |       " + mazoAuxiliar.get(2).getColor() + "     |       " + mazoAuxiliar.get(3).getColor());
-                System.out.println("|             |   |             |   |             |   |             |");
-                System.out.println(" -------------     -------------     -------------     -------------");
-                contador = 0;
-                mazoAuxiliar.clear();
+    //Muestra todas las cartas de la mesa
+    public static void mostrarCartas() {
+        List<List<String>> cartas = new ArrayList<>();
+        if (!juegoTerminado) {
+
+            // Obtiene las representaciones de todas las cartas del usuario
+            for (Carta carta : cartasUsuario) {
+                cartas.add(carta.mostrarCartaUsuario());
             }
 
+            // Imprime las cartas lado a lado
+            for (int i = 0; i < 5; i++) { // Cada carta tiene 5 líneas
+                for (List<String> c : cartas) {
+                    System.out.print(c.get(i));
+                }
+                System.out.println(); // Salto de línea después de imprimir todas las cartas en la misma fila
+            }
         }
     }
+
+    //Pedir un numero de carta
+    public static int pedirNumeroCarta() {
+        Scanner entrada = new Scanner(System.in);
+        int carta;
+        System.out.println("Por favor, elige la carta:");
+        carta = entrada.nextInt();
+
+        //Comprobamos si la carta existe si no la volvemos a pedir
+        if (carta < 0 || carta > (cartasUsuario.size() + 1)) {
+            System.out.println("ERROR.Introduce una carta válida.");
+            carta = pedirNumeroCarta();
+        }
+        return carta;
+    }
+
 }
